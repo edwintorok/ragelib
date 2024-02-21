edwintorok/ragelib

commit 69db96ccdcca8597be7253850ffe861938c08d33

Reorder element checking as to avoid 10s wait for dialogue pop-up

ragelib/image_fetcher.py
@@ -22,8 +22,10 @@ class ImageFetcher():
         self.logger.debug(f"Begun fetching url {url}")
         driver.get(url)
         
-        try:
-            WebDriverWait(driver, timeout=10).until(expected.alert_is_present())
+        wait.until(expected.visibility_of((By.ID, 'graph_title')))
+        self.logger.debug("Element #graph_title now visible, graph loading")
+        
+        if expected.alert_is_present():
             alert = driver.switch_to.alert
             if "About to plot" in alert.text:
                 alert.accept()
@@ -31,13 +33,9 @@ class ImageFetcher():
             else:
                 alert.dismiss()
                 self.logger.warn("Unexpected alert box dismissed")
-        except TimeoutException:
-            self.logger.debug("No alert box present")
             
         # On draw starting the progress_img element appears (spinner) and the graph title is unhidden. 
         # When done it disappears.
-        wait.until(expected.visibility_of_element_located((By.ID, 'graph_title')))
-        self.logger.debug("Element #graph_title now visible, graph loading")
         wait.until(expected.invisibility_of_element_located((By.ID, 'progress_img')))
         self.logger.debug("Element #progress_img now invisible, graph loaded")
 

commit 0a841cd1e5dfe1638a3cb5f70af83b63bc152ccf

Fix race condition on alert presence
`expected.alert_is_present()` is always true, because it is a Selenium predicate
(a `Callable` that expects a web-driver).

This currently raises a NoSuchAlertException.
We have to actually call the expectation with a driver, which will check if an alert is present,
or return false (it internally catches this exception), instead of calling switch_to ourselves.

Fixes: 69db96c ("Reorder element checking as to avoid 10s wait for dialogue pop-up")

Signed-off-by: Edwin Török <edwin.torok@cloud.com>

ragelib/image_fetcher.py
@@ -25,8 +25,8 @@ class ImageFetcher():
         wait.until(expected.visibility_of_element_located((By.ID, 'graph_title')))
         self.logger.debug("Element #graph_title now visible, graph loading")
         
-        if expected.alert_is_present():
-            alert = driver.switch_to.alert
+        alert = expected.alert_is_present()(driver)
+        if alert:
             if "About to plot" in alert.text:
                 alert.accept()
                 self.logger.warn("Many points alert box accepted")